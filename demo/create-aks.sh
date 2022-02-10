3e14ae99de6c9cd0c19d7c535b6bddc3d1c612a5
gperezivo <gperezi@vernegroup.com>
2022-02-10 17:05:43 +0100

    Añadidos pasos para login con AZ CLI

--- demo/create-aks.sh
+++ demo/create-aks.sh
@@ -1,4 +1,12 @@
+#Login en azure
+az login 
+#Seleccionar la subscripción
+az account set --subscription "Subscription ID"
+
+#Crear el grupo de recursos
 az group create openclass
+#Crear el cluster
 az aks create -g openclass -n aks-demos -l northeurope --node-count 1 
 
+#Obtener las credenciales para poder usar kubectl
 az aks get-credentials -g openclass -n aks-demos
